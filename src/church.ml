71a77c89fc11efc2b18ac1b33109f6f5b6ca64bf
hackwaly <hackwaly@gmail.com>
2016-05-20 18:36:00 +0800

    more church encoding

--- src/church.ml
+++ src/church.ml
@@ -14,14 +14,28 @@ let init_globals parse_exp =
     fun globals (k, v) ->
     StringMap.add k v globals
   ) StringMap.empty [
-    "plus",  parse_exp "λm.λn.λf.λx. m f (n f x)";
-    "succ",  parse_exp "λn.λf.λx. f (n f x)";
-    "mult",  parse_exp "λm.λn.λf. n (m f)";
-    "exp",   parse_exp "λm.λn. n m";
-    "pred",  parse_exp "λn.λf.λx. n (λg.λh. h (g f)) (λu. x) (λu. u)";
-    "true",  parse_exp "λa.λb. a";
-    "false", parse_exp "λa.λb. b";
-    "and",   parse_exp "λm.λn.λa.λb. m (n a b) b";
-    "or",    parse_exp "λm.λn.λa.λb. m a (n a b)";
-    "not",   parse_exp "λm.λa.λb. m b a";
+    "plus",     parse_exp "λm.λn.λf.λx. m f (n f x)";
+    "succ",     parse_exp "λn.λf.λx. f (n f x)";
+    "mult",     parse_exp "λm.λn.λf. n (m f)";
+    "exp",      parse_exp "λm.λn. n m";
+    "pred",     parse_exp "λn.λf.λx. n (λg.λh. h (g f)) (λu. x) (λu. u)";
+    "minus",    parse_exp "λm.λn. n pred m";
+    "true",     parse_exp "λa.λb. a";
+    "false",    parse_exp "λa.λb. b";
+    "and",      parse_exp "λp.λq. p q p";
+    "or",       parse_exp "λp.λq. p p q";
+    "not",      parse_exp "λp. p false true";
+    "if",       parse_exp "λp.λa.λb. p a b";
+    "is_zero",  parse_exp "λn. n (λx. false) true";
+    "leq",      parse_exp "λm.λn. is_zero (minus m n)";
+    "eq",       parse_exp "λm.λn. and (leq m n) (leq n m)";
+    "pair",     parse_exp "λx.λy.λz. z x y";
+    "first",    parse_exp "λp. p (λx.λy. x)";
+    "second",   parse_exp "λp. p (λx.λy. y)";
+    "nil",      parse_exp "pair true true";
+    "is_nil",   parse_exp "first";
+    "cons",     parse_exp "λh.λt. pair false (pair h t)";
+    "head",     parse_exp "λz. first (second z)";
+    "tail",     parse_exp "λz. second (second z)";
+    "Y",        parse_exp "λf. (λx. x x) (λx. f (x x))";
   ]
